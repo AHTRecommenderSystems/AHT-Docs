76f3c9d7bae53d841cdf9a2dc6ef1b006bc8d7ca
Toño Castro <tonoescom@gmail.com>
2015-11-08 22:29:19 -0600

    a item has been elimanted for duplicated

--- Resources/Categories.js
+++ Resources/Categories.js
@@ -1,91 +1,91 @@
-"id","type","name"
-1,"kind","Panes y masas"
-2,"kind","Pastas"
-3,"kind","Bizcochos y galletas"
-4,"kind","Carnes"
-5,"kind","Aves"
-6,"kind","Pescados y mariscos"
-7,"kind","Ensaladas"
-8,"kind","Contenido alcoholico"
-9,"kind","Salsas y guarniciones"
-10,"kind","Sopas y cremas"
-11,"kind","Arroces"
-12,"kind","Legumbres y guisos"
-13,"kind","Tartas y dulces"
-14,"kind","Helados y sorbetes"
-15,"kind","Frutas"
-16,"kind","Verduras"
-17,"kind","Huevos"
-18,"kind","Lacteos"
-19,"kind","Frutos secos"
-20,"kind","Encurtidos y conservas"
-21,"kind","Postre"
-22,"kind","Bebida"
-23,"kind","Primeros platos"
-24,"kind","Segundos platos"
-25,"kind","Entradas"
-26,"kind","Sopas y cremas"
-27,"kind","Acompa\~namientos"
-28,"kind","Emparedados"
-29,"kind","Botana"
-30,"flavour","Dulce"
-31,"flavour","Salado"
-32,"flavour","Agrio"
-33,"flavour","Amargo"
-34,"flavour","Umami"
-35,"flavour","Picante"
-36,"occasion","Halloween"
-37,"occasion","Navidad"
-38,"occasion","San Valentin"
-39,"occasion","Primavera"
-40,"occasion","Verano"
-41,"occasion","Oto\~no"
-42,"occasion","Invierno"
-43,"occasion","Desayunos"
-44,"occasion","Almuerzos"
-45,"occasion","Meriendas"
-46,"occasion","Ocasion especial"
-47,"region","Italiana"
-48,"region","Mediterranea"
-49,"region","Asiatica"
-50,"region","Mexicana"
-51,"region","Americana"
-52,"region","Hindu"
-53,"region","Francesa"
-54,"region","Tailandesa"
-55,"region","Cantonesa"
-56,"region","Japonesa"
-57,"region","China"
-58,"region","Medio oriente"
-59,"region","Alemana"
-60,"region","Argentina"
-61,"region","Brasile\~na"
-62,"region","Colombiana"
-63,"region","Coreana"
-64,"region","Cubana"
-65,"region","Espa\~nola"
-66,"region","Finlandesa"
-67,"region","Griega"
-68,"region","Holandesa"
-69,"region","Indonesa"
-70,"region","Portuguesa"
-71,"health","Bajas en colesterol"
-72,"health","Diabeticos"
-73,"health","Sin lactosa"
-74,"health","Celiacos"
-75,"health","Alergicos"
-76,"health","Bajar de peso"
-77,"health","Vegetarianos"
-78,"temperature","Frio"
-79,"temperature","Templado"
-80,"temperature","Caliente"
-81,"people","Bebes"
-82,"people","Ni\~nos"
-83,"people","Adultos"
-84,"people","Familiares"
-85,"people","Adultos mayores"
-86,"texture","Liquidas"
-87,"texture","Blandas"
-88,"texture","Semi-blandas"
-89,"texture","Duras"
-90,"texture","Crujientes"
+id,type,name
+1,kind,Panes y masas
+2,kind,Pastas
+3,kind,Bizcochos y galletas
+4,kind,Carnes
+5,kind,Aves
+6,kind,Pescados y mariscos
+7,kind,Ensaladas
+8,kind,Contenido alcoholico
+9,kind,Salsas y guarniciones
+10,kind,Sopas y cremas
+11,kind,Arroces
+12,kind,Legumbres y guisos
+13,kind,Tartas y dulces
+14,kind,Helados y sorbetes
+15,kind,Frutas
+16,kind,Verduras
+17,kind,Huevos
+18,kind,Lacteos
+19,kind,Frutos secos
+20,kind,Encurtidos y conservas
+21,kind,Postre
+22,kind,Bebida
+23,kind,Primeros platos
+24,kind,Segundos platos
+25,kind,Entradas
+26,kind,Acompa\~namientos
+27,kind,Emparedados
+28,kind,Botana
+29,flavour,Dulce
+30,flavour,Salado
+31,flavour,Agrio
+32,flavour,Amargo
+33,flavour,Umami
+34,flavour,Grasoso
+35,flavour,Picante
+36,occasion,Halloween
+37,occasion,Navidad
+38,occasion,San Valentin
+39,occasion,Primavera
+40,occasion,Verano
+41,occasion,Oto\~no
+42,occasion,Invierno
+43,occasion,Desayunos
+44,occasion,Almuerzos
+45,occasion,Meriendas
+46,occasion,Ocasion especial
+47,region,Italiana
+48,region,Mediterranea
+49,region,Asiatica
+50,region,Mexicana
+51,region,Americana
+52,region,Hindu
+53,region,Francesa
+54,region,Tailandesa
+55,region,Cantonesa
+56,region,Japonesa
+57,region,China
+58,region,Medio oriente
+59,region,Alemana
+60,region,Argentina
+61,region,Brasile\~na
+62,region,Colombiana
+63,region,Coreana
+64,region,Cubana
+65,region,Espa\~nola
+66,region,Finlandesa
+67,region,Griega
+68,region,Holandesa
+69,region,Indonesa
+70,region,Portuguesa
+71,health,Bajas en colesterol
+72,health,Diabeticos
+73,health,Sin lactosa
+74,health,Celiacos
+75,health,Alergicos
+76,health,Bajar de peso
+77,health,Vegetarianos
+78,temperature,Frio
+79,temperature,Templado
+80,temperature,Caliente
+81,people,Bebes
+82,people,Ni\~nos
+83,people,Adultos
+84,people,Familiares
+85,people,Adultos mayores
+86,texture,Liquidas
+87,texture,Blandas
+88,texture,Semi-blandas
+89,texture,Duras
+90,texture,Crujientes
